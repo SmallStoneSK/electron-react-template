17a2b7da5447f1154519cd908e29d908b0154cc5
xst <575913857@qq.com>
2019-03-27 10:00:28 +0800

    【modify】replace link address

--- src/renderer/components/Body.js
+++ src/renderer/components/Body.js
@@ -8,7 +8,7 @@ export class Body extends React.PureComponent {
       <div className={'body-container'}>
         <img className={'author-head-img'} src={require('../assets/images/head.jpg')}/>
         <p className={'content-text'}>
-          Hello, if you want to learn more, click <a className={'href-text'} href={'https://github.com/SmallStoneSK'} target={'blank'}>here</a>.
+          Hello, if you want to learn more, click <a className={'href-text'} href={'https://github.com/SmallStoneSK/electron-react-template'} target={'blank'}>here</a>.
         </p>
       </div>
     );
